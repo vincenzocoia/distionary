002fabdb6b1f1e3cb1bca7ad32f12936d39c6bbc
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-11-09 20:00:57 -0800

    Update dst_geom.R

--- R/dst_geom.R
+++ R/dst_geom.R
@@ -3,17 +3,17 @@
 #' Makes a distribution belonging to the family of
 #' geometric distributions.
 #'
-#' @param p probability of success in each trial. 0 < p <= 1
+#' @param prob probability of success in each trial. 0 < p <= 1
 #'
 #' @examples
 #' dst_geom(0.4)
 #'
 #' @export
-dst_geom <- function(p){
-  if(p < 0 | p > 1){
-    stop('p must be within 0 and 1')
+dst_geom <- function(prob){
+  if(prob < 0 | prob > 1){
+    stop('prob must be within 0 and 1')
   }
   dst_parametric("geom",
-                 prob = p,
+                 prob = prob,
                  .variable = "discrete")
 }
