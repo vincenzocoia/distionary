0138709e24637da5416cd2e49f48496884d89775
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-11-09 20:00:49 -0800

    Update dst_binom.R

--- R/dst_binom.R
+++ R/dst_binom.R
@@ -4,20 +4,20 @@
 #' binomial distributions.
 #'
 #' @param size number of trials
-#' @param p success probability for each trial
+#' @param prob success probability for each trial
 #'
 #' @examples
 #' dst_binom(10, 0.6)
 #'
 #' @export
-dst_binom <- function(size, p){
+dst_binom <- function(size, p=prob){
   if(size < 0){
     stop("Size must be non-negative")
   }
-  if (p < 0 | p > 1){
-    stop('p must be within 0 and 1')
+  if (prob < 0 | prob > 1){
+    stop('prob must be within 0 and 1')
   }
   dst_parametric("binom",
-                 size = size, prob = p,
+                 size = size, prob = prob,
                  .variable = "discrete")
 }
