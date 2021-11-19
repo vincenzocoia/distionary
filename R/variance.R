ea12f1e2e758fb78542b29ca8ba3054b859bbe79
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-11-18 20:50:02 -0800

    update variance.R (increase subdivisions)
    increase the number of allowed subdivisions to resolve "Integrating error: maximum number of subdivisions reached"

--- R/variance.R
+++ R/variance.R
@@ -9,6 +9,6 @@ variance.dst <- function(distribution) {
   mu <- mean(distribution)
   sf <- representation_as_function(distribution, "survival")
   sf2 <- function(t) 1 + sf(mu + sqrt(t)) - sf(mu - sqrt(t))
-  int <- stats::integrate(sf2, 0, Inf)
+  int <- stats::integrate(sf2, 0, Inf, subdivisions=2000)
   int$value
 }
