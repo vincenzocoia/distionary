99bf8beabbfd802accfe20c6bb50827754b0f601
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-11-18 22:16:59 -0800

    Update representations-dst_gev.R

--- R/representations-dst_gev.R
+++ R/representations-dst_gev.R
@@ -1,67 +1,58 @@
 #' @export
 eval_cdf.gev <- function(distribution, at) {
+  r <- range(distribution)
+  left_at <- at[at < r]
+  right_at <- at[at > r]
   with(parameters(distribution), {
-    s = (at - location) / scale
-    if (shape == 0) {
-      res <- exp(-exp(- s))
-      res
-    } else {
-      if (shape * s > -1) {
-        res <- exp(-(1 + shape * s) ^ (-1 / shape))
-        res
-      } else {
-        if (shapen > 0) {
-          res <- 0
-          res
-        } else {
-          res <- 1
-          res
-        }
-      }
-    }
+    t <- gev_t_function(at, location = location, scale = scale, shape = shape)
+    res <- exp(-t)
+    res[left_at] <- 0
+    res[right_at] <- 1
+    res
   })
 }
 
 #' @export
-eval_quantile.gev <- function(distribution, at, ...) {
+eval_quantile.gev <- function(distribution, at) {
   with(parameters(distribution), {
-    invalid <- at < 0 | at > 1
+    invalid_at <- at < 0 | at > 1
     if (shape == 0) {
-      res <- location - scale * log(- log(p) )
-      res[invalid] <- NaN
-      res
+      res <- location - scale * log(-log(at))
     } else {
-      if (shape > 0) {
-        invalid_at <- at == 1
-        res <- location + scale * ((- log(p)) ^ (- shape) - 1) / shape
-        res[invalid_at] <- NaN
-        res
-      } else {
-        invalid_at <- at == 0
-        res <- location + scale * ((- log(p)) ^ (- shape) - 1) / shape
-        res[invalid_at] <- NaN
-        res
-      }
+      res <- location + scale * ((-log(at))^(-shape) - 1) / shape
     }
+    res[invalid_at] <- NaN
+    res
   })
 }
 
 #' @export
 eval_density.gev <- function(distribution, at, strict = TRUE) {
+  r <- range(distribution)
+  left_at <- at[at < r]
+  right_at <- at[at > r]
   with(parameters(distribution), {
-    s = (at - location) / scale
-    if (shape == 0) {
-      res <- d = log( 1 / scale) - s - exp(- s)
-      res
-    } else {
-      if (shape * s > -1) {
-        ss <- 1 + shape * s
-        sp <- 1 / shape
-        res <- ss^(- (1 + sp)) * exp(- ss^(- sp))
-        res
-      } else {
-        res <- 0
-      }
-    }
+    t <- gev_t_function(at, location = location, scale = scale, shape = shape)
+    res <- t^(shape + 1) / scale * exp(-t)
+    res[left_at] <- 0
+    res[right_at] <- 0
+    res
   })
+}
+
+#' 't()' function for calculating GEV quantities
+#'
+#' @param x Argument of the function; vectorized.
+#' @inheritParams dst_gev
+#' @seealso See the Wikipedia entry for the GEV distribution,
+#' https://en.wikipedia.org/wiki/Generalized_extreme_value_distribution
+#' @note The shape parameter is not vectorized. This function is only
+#' intended to be used when location, scale, and shape are scalars.
+gev_t_function <- function(x, location, scale, shape) {
+  z <- (x - location) / scale
+  if (shape == 0) {
+    exp(-z)
+  } else {
+    (1 + shape * z)^(-1 / shape)
+  }
 }
