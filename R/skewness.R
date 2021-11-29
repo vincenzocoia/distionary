968efef8d8abeb12fbb92d94497c8ce16c8d796f
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-11-28 16:36:23 -0800

    add subdivisions=2000 to skewness for temporary fix

--- R/skewness.R
+++ R/skewness.R
@@ -15,7 +15,7 @@ skewness.dst <- function(distribution) {
   one_minus_flipped <- function(t) 1 - sf(mu - t^(1 / 3))
   # (flipped about t=0 because (-1)^(1/3) returns a complex root of
   #  unity, or NaN, instead of the real one, -1.)
-  pos_int <- stats::integrate(sf2, 0, Inf)
-  neg_int <- stats::integrate(one_minus_flipped, 0, Inf)
+  pos_int <- stats::integrate(sf2, 0, Inf, subdivisions=2000)
+  neg_int <- stats::integrate(one_minus_flipped, 0, Inf, subdivisions=2000)
   (pos_int$value - neg_int$value) / sigma^3
 }
