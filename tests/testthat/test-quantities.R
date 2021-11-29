bfcb88a0f83af4829a417f26f6aa44701c93d24f
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-11-28 16:37:24 -0800

    Update test-quantities.R

--- tests/testthat/test-quantities.R
+++ tests/testthat/test-quantities.R
@@ -32,20 +32,43 @@ check_quantity <- function(distribution, name, tolerance = 1e-3) {
 
 test_that("quantities align with numeric computations.", {
   distributions <- list(
-    dst_norm(0, 1),
-    dst_norm(4, 2),
-    dst_gpd(0, 1, 1 / 4 - 0.01),
-    dst_gpd(0, 1, 1 / 3 - 0.01),
-    dst_gpd(0, 1, 1 / 2 - 0.01),
-    dst_gpd(0, 1, 1 - 0.01),
-    dst_gpd(0, 1, 1 + 0.01),
-    dst_gpd(0, 1, 0),
-    dst_gpd(0, 1, -1),
-    dst_exp(1),
-    dst_exp(2)
-    # dst_weibull(1, 1),
-    # dst_weibull(1.1, 2.1),
-    # dst_weibull(0.85, 3.55)
+    # dst_norm(0, 1),
+    # dst_norm(4, 2),
+    # dst_pois(1),
+    # dst_pois(3),
+    # dst_pois(5),
+    # dst_unif(0, 1),
+    # dst_unif(-1, 1),
+    # dst_unif(-2, 0.8),
+    # dst_beta(2,3),
+    # dst_beta(0.5,0.9),
+    # dst_binom(10, 0.6),
+    # dst_binom(8, 0.05),
+    # dst_binom(2, 0.89),
+    # dst_nbinom(6, 0.05), # Error: the integral is probably divergent
+    # dst_nbinom(3, 0.87),
+    # dst_nbinom(1.5, 0.5),
+    # dst_gpd(0, 1, 1 / 4 - 0.01),
+    # dst_gpd(0, 1, 1 / 3 - 0.01),
+    # dst_gpd(0, 1, 1 / 2 - 0.01),
+    # dst_gpd(0, 1, 1 - 0.01),
+    # dst_gpd(0, 1, 1 + 0.01),
+    # dst_gpd(0, 1, 0),
+    # dst_gpd(0, 1, -1)
+    # dst_exp(1),
+    # dst_exp(2),
+    # dst_geom(0.5),
+    # dst_geom(0.2),
+    # dst_geom(1)
+    # dst_geom(0.05) #seems it will raise "Error: the integral is probably divergent" every time we used 0.05
+    # dst_chisq(3), # remove hard-coded median for now
+    # dst_chisq(5),
+    # dst_chisq(1),
+    # dst_cauchy(0, 1) # not fixed yet
+    dst_weibull(1, 1),
+    dst_weibull(2, 2)
+    # dst_weibull(1.1, 2.1)
+    #dst_weibull(0.85, 3.55)
     # dst_f(3, 1),
     # dst_f(4, 1),
     # dst_f(6, 1),
@@ -55,7 +78,8 @@ test_that("quantities align with numeric computations.", {
     # dst_t(2),
     # dst_t(3),
     # dst_t(4),
-    # dst_t(4.2)
+    # dst_t(4.2),
+
   )
   for (d in distributions) {
     check_quantity(d, "mean")
