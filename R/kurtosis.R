e28af7610b0bd562776ab052d06e087f31c52a9b
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2021-12-03 14:29:02 -0800

    Update kurtosis.R

--- R/kurtosis.R
+++ R/kurtosis.R
@@ -16,11 +16,11 @@ kurtosis_raw.dst <- function(distribution) {
 }
 
 #' @export
-kurtosis_exc.dst <- function(distribution) {
+kurtosis_exc.dst <- function(distribution, ...) {
   mu <- mean(distribution)
   var <- variance(distribution)
   sf <- representation_as_function(distribution, "survival")
   sf2 <- function(t) 1 + sf(mu + t^(1 / 4)) - sf(mu - t^(1 / 4))
-  int <- stats::integrate(sf2, 0, Inf, subdivisions=2000)
+  int <- stats::integrate(sf2, 0, Inf, ...)
   int$value / var^2 - 3
 }
