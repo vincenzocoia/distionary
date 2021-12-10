8d4caf641393062941b7f4a32741f2de15f0d1f3
zhuzp98 <jsyxzzp@outlook.com>
2021-12-09 20:11:02 -0800

    Update test-quantities.R

--- tests/testthat/test-quantities.R
+++ tests/testthat/test-quantities.R
@@ -32,28 +32,28 @@ check_quantity <- function(distribution, name, tolerance = 1e-3) {
 
 test_that("quantities align with numeric computations.", {
   distributions <- list(
-    dst_norm(0, 1),
-    dst_norm(4, 2),  # Passed
-    dst_pois(1),
-    dst_pois(3),
-    dst_pois(5),  # Passed
-    dst_unif(0, 1),
-    dst_unif(-1, 1),
-    dst_unif(-2, 0.8), # Passed
-    dst_beta(2,3),
-    dst_beta(0.5,0.9), # Passed
-    dst_binom(10, 0.6),
-    dst_binom(8, 0.05),
-    dst_binom(2, 0.89), # Passed
-    dst_nbinom(10, 0.5), # Passed
-    dst_nbinom(6, 0.05), #
-    dst_nbinom(3, 0.87),
-    dst_nbinom(1.5, 0.5), # Passed
-    dst_geom(0.4),
-    dst_geom(1),
-    dst_geom(0.05), # Passed
-    dst_exp(1),
-    dst_exp(2), # Passed
+    #dst_norm(0, 1),
+    #dst_norm(4, 2),  # Passed
+    #dst_pois(1),
+    #dst_pois(3),
+    #dst_pois(5),  # Passed
+    #dst_unif(0, 1),
+    #dst_unif(-1, 1),
+    #dst_unif(-2, 0.8), # Passed
+    #dst_beta(2,3),
+    #dst_beta(0.5,0.9), # Passed
+    #dst_binom(10, 0.6),
+    #dst_binom(8, 0.05),
+    #dst_binom(2, 0.89), # Passed
+    #dst_nbinom(10, 0.5), # Passed
+    #dst_nbinom(6, 0.05), #
+    #dst_nbinom(3, 0.87),
+    #dst_nbinom(1.5, 0.5), # Passed
+    #dst_geom(0.4),
+    #dst_geom(1),
+    #dst_geom(0.05), # Passed
+    #dst_exp(1),
+    #dst_exp(2), # Passed
     # dst_weibull(1, 1),
     # dst_weibull(1.1, 2.1),
     # dst_weibull(0.85, 3.55)
@@ -61,24 +61,24 @@ test_that("quantities align with numeric computations.", {
 
 
 
-    dst_gpd(0, 1, 1 / 4 - 0.01),
-    dst_gpd(0, 1, 1 / 3 - 0.01),
-    dst_gpd(0, 1, 1 / 2 - 0.01),
-    dst_gpd(0, 1, 1 - 0.01),
-    dst_gpd(0, 1, 1 + 0.01),
-    dst_gpd(0, 1, 0),
-    dst_gpd(0, 1, -1),
+    #dst_gpd(0, 1, 1 / 4 - 0.01),
+    #dst_gpd(0, 1, 1 / 3 - 0.01),
+    #dst_gpd(0, 1, 1 / 2 - 0.01),
+    #dst_gpd(0, 1, 1 - 0.01),
+    #dst_gpd(0, 1, 1 + 0.01),
+    #dst_gpd(0, 1, 0),
+    #dst_gpd(0, 1, -1)
 
-    #dst_f(3, 1),
+    dst_f(3, 6)
     #dst_f(4, 1),
     #dst_f(6, 1),
     #dst_f(8, 1),
     #dst_f(8.2, 1),
-    dst_t(1),
-    dst_t(2),
-    dst_t(3),
-    dst_t(4),
-    dst_t(4.2)
+    #dst_t(1),
+    #dst_t(2),
+    #dst_t(3),
+    #dst_t(4),
+    #dst_t(4.2)
   )
   for (d in distributions) {
     print(d$name)
