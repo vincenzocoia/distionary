5cfa2f4a5005dd640334d1e32c86da5abe1fb533
Zhipeng Zhu <jsyxzzp@outlook.com>
2022-02-04 13:21:39 -0800

    Comments on test-quantiles.
    Add description summaries for tests.

--- tests/testthat/test-quantities.R
+++ tests/testthat/test-quantities.R
@@ -40,63 +40,61 @@ check_quantity <- function(distribution, name, tolerance = 1e-3,
 
 test_that("quantities align with numeric computations.", {
   distributions <- list(
-    dst_norm(0, 1),
-    dst_norm(4, 2),
-    dst_pois(1),
-    dst_pois(3),
-    dst_pois(5),
-    dst_unif(0, 1),
-    dst_unif(-1, 1),
-    dst_unif(-2, 0.8)
-    # dst_beta(2,3) # Warning: "skewness" and "kurtosis_exc" errors from integral (need more rounding)
-    # dst_beta(0.5,0.9),
-    # dst_binom(10, 0.6)
-    # dst_binom(8, 0.05),
-    # dst_binom(2, 0.89),
-    # dst_nbinom(6, 0.05) # Error: the integral is probably divergent
-    # dst_nbinom(3, 0.87),
-    # dst_nbinom(1.5, 0.5),
-    # dst_gpd(0, 1, 1 / 4 - 0.01)
-    # dst_gpd(0, 1, 1 / 3 - 0.01),
-    # dst_gpd(0, 1, 1 / 2 - 0.01),
-    # dst_gpd(0, 1, 1 - 0.01),
-    # dst_gpd(0, 1, 1 + 0.01),
-    # dst_gpd(0, 1, 0),
-    # dst_gpd(0, 1, -1)
-    # dst_exp(1)
-    # dst_exp(2),
-    # dst_geom(0.5)
-    # dst_geom(0.2),
-    # dst_geom(1)
-    # dst_geom(0.05) #seems it will raise "Error: the integral is probably divergent" every time we used 0.05
-    # dst_chisq(3), # remove hard-coded median for now
-    # dst_chisq(5),
-    # dst_chisq(1)
-    # dst_cauchy(0, 1) # not fixed yet 
-                      # "Mean": Quantities align with numeric computations. 
-                      # "Skewness": Integral did not converge. 
-    # dst_weibull(1, 1)
-    # dst_weibull(2, 2)
-    # dst_weibull(1.1, 2.1)
-    # dst_weibull(0.85, 3.55)
-    # dst_f(3, 1) # "Mean": Integral did not converge.
-    # dst_f(4, 1),
-    # dst_f(6, 1),
-    # dst_f(8, 1),
-    # dst_f(8.2, 1),
-    # dst_t(1) # need fixed. "Mean"
-    # dst_t(2),
-    # dst_t(3),
-    # dst_t(4),
-    # dst_t(4.2),
+    # dst_norm(0, 1),               ##***##
+    # dst_norm(4, 2)                ##***##
+    # dst_pois(1),                  ##***##
+    # dst_pois(3),                  ##***##
+    # dst_pois(5)                   ##***##
+    # dst_unif(0, 1),               ##***##
+    # dst_unif(-1, 1),              ##***##
+    # dst_unif(-2, 0.8)             ##***##
+    # dst_beta(2,3),                ##***##
+    # dst_beta(0.5,0.9)             ##***##
+    # dst_binom(10, 0.6),           ##***##
+    # dst_binom(8, 0.05),           ##***##
+    # dst_binom(2, 0.89)            ##***##
+    # dst_nbinom(6, 0.05)           # skewness: 1 vs NaN; kurtosis_exc: the integral is probably divergent
+    # dst_nbinom(3, 0.87)           # kurtosis_exc: the integral is probably divergent
+    # dst_nbinom(1.5, 0.5)          ##***##
+    # dst_gpd(0, 1, 1 / 4 - 0.01),  ##***##
+    # dst_gpd(0, 1, 1 / 3 - 0.01)   ##***##
+    # dst_gpd(0, 1, 1 / 2 - 0.01)   # skewness: Integral did not converge
+    # dst_gpd(0, 1, 1 - 0.01)       # skewness: Integral did not converge
+    # dst_gpd(0, 1, 1 + 0.01)       # mean/skewness: Integral did not converge.(Mean exists when param3 < 1)
+    # dst_gpd(0, 1, 0)              ##***##
+    # dst_gpd(0, 1, -1)             ##***##
+    # dst_exp(1)                    ##***##
+    # dst_exp(2)                    ##***##
+    # dst_geom(0.5)                 ##***##
+    # dst_geom(0.2)                 ##***##
+    # dst_geom(1)                   # skewness/kurtosis_exc: need fixed.
+    # dst_geom(0.05)                # kurtosis_exc: "Error: the integral is probably divergent" every time we used 0.05
+    # dst_chisq(3),                 ##***## remove hard-coded median for now
+    # dst_chisq(5),                 ##***##
+    # dst_chisq(1)                  ##***##
+    # dst_cauchy(0, 1)              # mean: need fixed. skewness: Integral did not converge. 
+    # dst_weibull(1, 1)             ##***##
+    # dst_weibull(2, 2)             # kurtosis_exc: -8 vs 0
+    # dst_weibull(1.1, 2.1)         # kurtosis_exc: -9 vs 4
+    # dst_weibull(0.85, 3.55)       # kurtosis_exc: -5 vs 10
+    # dst_f(3, 1)                   # mean/skewness: Integral did not converge. median: skip
+    # dst_f(4, 1)                   # mean/skewness: Integral did not converge. median: skip
+    # dst_f(6, 1),                  # mean/skewness: Integral did not converge. median: skip
+    # dst_f(8, 1),                  # mean/skewness: Integral did not converge. median: skip
+    # dst_f(8.2, 1)                 # mean/skewness: Integral did not converge. median: skip
+    # dst_t(1)                      # mean: need fixed. skewness: Integral did not converge.
+    # dst_t(2),                     # mean: need fixed. skewness: Integral did not converge.
+    # dst_t(3),                     # mean: need fixed. skewness: Integral did not converge.
+    # dst_t(4),                     # mean: need fixed. skewness: Integral did not converge.
+    # dst_t(4.2),                   # mean: need fixed. skewness: Integral did not converge.
   )
   for (d in distributions) {
-    # print(d$name)
+    print(d$name)
     check_quantity(d, "mean")
     check_quantity(d, "variance")
-    #check_quantity(d, "skewness")
-    #check_quantity(d, "kurtosis_exc", verbose = TRUE)
-    #check_quantity(d, "median")
+    check_quantity(d, "skewness")
+    check_quantity(d, "kurtosis_exc", verbose = TRUE)
+    check_quantity(d, "median")
   }
 })
 
